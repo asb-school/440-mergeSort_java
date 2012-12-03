f19ab99b54571c66c13432c01fcbde86b30b1a98
asb-school <asb@andrews.edu>
2012-12-03 15:41:25 -0500

    new changes

--- src/mergesort/MergeSort.java
+++ src/mergesort/MergeSort.java
@@ -9,58 +9,82 @@
 package mergesort;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MergeSort 
 {
-	public static void main(String[] args) 
-	{
-        		// Variables
-		int collectionSize = 0;
-		int numberOfThreads = 0;
+        public static Vector<Integer> masterItemCollection = new Vector<Integer>();
 
-		ArrayList<Integer> masterItemCollection = new ArrayList<Integer>();
-		ArrayList<Sorter> sorterList = new ArrayList<Sorter>();
+        // Function to generate random numbers
+        public static void generateIntegers(int givenCollectionSize)
+        {
+                // Create a random generager
+                Random randomNumberGenerator = new Random();
 
-        		// Get command line parameters
-		if (args.length == 2)
-		{
-        			collectionSize = Integer.parseInt(args[0]);
-        			numberOfThreads = Integer.parseInt(args[1]);
-        		}
-        		// Bad parameters
-        		else
-        		{
-        			System.err.println("You really dun goofd");
-        			System.exit(0);
-        		}
+                // For each 
+                for (int i = 0; i < givenCollectionSize; i++)
+                {
+                        // Generate numbers between 0 and 1000
+                        masterItemCollection.add(randomNumberGenerator.nextInt(1000));
+                }
+        }
 
 
+        public static void main(String[] args) 
+        {
+                // Variables
+                int collectionSize = 0;
+                int numberOfThreads = 0;
+                
+                ArrayList<Sorter> sorterList = new ArrayList<Sorter>();
 
-        		// Executor service
-        		ExecutorService threadScheduler = Executors.newFixedThreadPool(numberOfThreads);
+                // Get command line parameters
+                if (args.length == 2)
+                {
+                        collectionSize = Integer.parseInt(args[0]);
+                        numberOfThreads = Integer.parseInt(args[1]);
+                }
+                // Bad parameters
+                else
+                {
+                        System.err.println("You really dun goofd");
+                        System.exit(0);
+                }
+                
+                // Generate random numbers in the master item collection
+                generateIntegers(collectionSize);
 
-        		for (int i = 0; i < numberOfThreads; i++)
-        		{
-        			// Create new sorter
-        			Sorter mergeSorter = new Sorter(masterItemCollection, 3, 3);
+                // Executor service
+                ExecutorService threadScheduler = Executors.newFixedThreadPool(numberOfThreads);
 
-        			// Keep track of the sorter
-        			sorterList.add(mergeSorter);
+                for (int iteratorIndex = 1; iteratorIndex <= numberOfThreads; iteratorIndex++)
+                {
+                        // Create new sorter
+                        Sorter mergeSorter = new Sorter(masterItemCollection, collectionSize, numberOfThreads, iteratorIndex);
 
-        			// Add item to thread scheduler
-        			threadScheduler.execute(mergeSorter);
-        		}
+                        // Keep track of the sorter
+                        sorterList.add(mergeSorter);
 
-        		// Tell the threadScheduler that there are no more things to process and it should shutdown
-		// and stop expecting new tasks to process
-	 	threadScheduler.shutdown();
+                        // Add item to thread scheduler
+                        threadScheduler.execute(mergeSorter);
+                }
 
-	 	// Hold/pause main method (this one) execution until the threadScheduler is completed
-		while (!threadScheduler.isTerminated()) {}
+                // Tell the threadScheduler that there are no more things to process and it should shutdown
+                // and stop expecting new tasks to process
+                threadScheduler.shutdown();
 
-		// Merge items back together
+                // Hold/pause main method (this one) execution until the threadScheduler is completed
+                while (!threadScheduler.isTerminated()) {}
 
-	}
+                // Merge items back together
+
+                /*
+
+                
+
+                */
+        }
 }
--- src/mergesort/Sorter.java
+++ src/mergesort/Sorter.java
@@ -9,22 +9,82 @@
 package mergesort;
 
 import java.util.ArrayList;
+import java.util.Vector;
 
 public class Sorter implements Runnable
 {
-	private ArrayList<Integer> sortedItemsCollection;
-	private ArrayList<Integer> masterItemCollection;
+	private ArrayList<Integer> workingItemCollection;
+	private Vector<Integer> masterItemCollection;
 
-	public Sorter(ArrayList<Integer> givenMasterItemCollection, Integer givenNumItems, Integer givenStartIndex)
+	private int numberOfItems;
+	private int numberOfThreads;
+	private int threadId;
+
+	private int beginingPosition;
+	private int endingPosition;
+
+
+	// --------------------------------------------------------------
+	// CONSTRUCTOR
+	
+	public Sorter(Vector<Integer> givenMasterItemCollection, Integer givenNumberOfItems, Integer givenNumberOfThreads, Integer givenThreadId)
 	{
 		// Dependency injection
 	    	this.masterItemCollection = givenMasterItemCollection;
 
+	    	// Set variables
+	    	this.numberOfItems = givenNumberOfItems;
+	    	this.numberOfThreads = givenNumberOfThreads;
+	    	this.threadId = givenThreadId;
+
+	    	// Initialize 
+	    	workingItemCollection = new ArrayList<Integer>();
 	}
+
+
+	// --------------------------------------------------------------
+	// CALCULATE POSITIONS
+
+	private void calculatePositions()
+	{
+		this.beginingPosition = (this.numberOfItems / this.numberOfThreads) * this.threadId;
+
+		this.endingPosition = this.beginingPosition + (this.numberOfItems / this.numberOfThreads);
+
+		// The last thread gets the slack
+		if ((this.threadId == this.numberOfThreads - 1) && (this.endingPosition < this.numberOfItems -1))
+		{
+		        this.endingPosition = this.numberOfItems - 1;
+		}
+	}
+
+
+	// --------------------------------------------------------------
+	// GET SORTED ITEMS
+
+	public ArrayList getSortedItems()
+	{
+		return this.workingItemCollection;
+	}
+
+
+	// --------------------------------------------------------------
+	// MAIN FUNCTION
 
 	@Override
 	public void run()
 	{
-		// sort here
+		// Calculate beginning and ending positions
+		this.calculatePositions();
+
+		// For each - but use beginning and ending positions
+		for (int iteratorIndex = this.beginingPosition; iteratorIndex <= this.endingPosition; iteratorIndex++)
+		{
+			// Copy to temporary local array
+			this.workingItemCollection.add(this.masterItemCollection.get(iteratorIndex));
+		}
+
+		// Sort working item collection
+		Collections.sort(this.workingItemCollection);
 	}
 }
